0b35c758221a3b59147c3677b251e915b14c4c32
kawyua <mkawyua@gmail.com>
2020-12-29 20:51:26 -0500

    changed password files to env

--- secrets.py
+++
@@ -1,5 +0,0 @@
-
-CLIENT_ID = 'o469c0y0939m05ujrohitu8xbm0mxo'
-CILENT_SECRET = 'hzvqdadf49uv0kolw3t2h7ssbcl8ek'
-SQLALCHEMY_DATABASE_URI = 'postgres://cjciqcfevduwtn:de85e51a708626bde9ce371573500537ba46b07e16e052e3dae9532a3522b3b2@ec2-54-235-158-17.compute-1.amazonaws.com:5432/dfrbf2pau7mro6'
-POSTGRESQL_DATABASE_URI = 'postgresql://postgres:kawyua@localhost/lexus'
